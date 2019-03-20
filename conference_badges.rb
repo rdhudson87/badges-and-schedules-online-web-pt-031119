539fb54e26f2dc6e38ce9001ee8e674f62f94638
Ricardo H <ricardo.hudson923@gmail.com>
2019-03-20 20:49:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,5 +9,5 @@ end
 batch_badge_creator = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 counter = 1
 batch_badge_creator.each do |assign_rooms|
-  puts "Hello, #{batch_badge_creator}! You'll be assigned to room #{counter}"
+  puts "Hello, #{batch_badge_creator}! You'll be assigned to room #{counter}!"
   counter += 1
