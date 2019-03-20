76ae3bd5abc0bc7df9a094a99fbac56f46bad9c9
Ricardo H <ricardo.hudson923@gmail.com>
2019-03-20 20:50:48 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+batch_badge_creator = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+counter = 1
+batch_badge_creator.each do |badge_maker|
+  puts "Hello, My name is #{batch_badge_creator}"
+  counter += 1
+end
+
+batch_badge_creator = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+counter = 1
+batch_badge_creator.each do |assign_rooms|
+  puts "Hello, #{batch_badge_creator}! You'll be assigned to room #{counter}!"
+  counter += 1
