c39167806a3e69d98290544d215166ec120cbcda
Ricardo H <ricardo.hudson923@gmail.com>
2019-03-20 20:47:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,6 @@ batch_badge_creator = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Ma
 counter = 1
 batch_badge_creator.each do |badge_maker|
   puts "Hello, My name is #{batch_badge_creator}"
-  puts "Stop hitting yourself #{brother}!"
   counter += 1
 end
+
